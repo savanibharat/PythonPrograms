904b569e26770e3493b1a531566395cec74c37dc
savanibharat <savanibharat@gmail.com>
2014-01-19 15:23:45 -0800

    Python print and if/else program

---
+++ com/Core/Python/__init__.py
@@ -0,0 +1,37 @@
+print "hello world";
+print("\"hello world\"");
+print(type("\"hello world\""));
+print 3+2;
+print type(3-2);
+print 3%2;
+print 3/2;
+print 3*2;
+print type('1'+'2');
+print ('1'+'2')
+print "Bharat"+" Savani"
+
+a=3+2;
+b=a;
+print b;
+
+if(a>0):
+    print "a>0"
+else:
+    print "a<0"
+
+
+"""hello world
+"hello world"
+<type 'str'>
+5
+<type 'int'>
+1
+1
+6
+<type 'str'>
+12
+Bharat Savani
+5
+a>0
+
+"""
