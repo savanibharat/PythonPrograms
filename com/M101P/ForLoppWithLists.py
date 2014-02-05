e608f4f5cd47495da52778cfe0388c8285fc1e8a
savanibharat <savanibharat@gmail.com>
2014-02-04 21:18:21 -0800

    For loop with lists and few python commands

---
+++ com/M101P/ForLoppWithLists.py
@@ -0,0 +1,23 @@
+'''
+Created on Feb 4, 2014
+
+@author: Savani Bharat
+'''
+fruits = ['apple', 'banana', 'orange']
+
+new_fruit = []
+
+for item in fruits:
+    
+    print item
+    
+    new_fruit.append(item)
+    
+print new_fruit
+
+'''
+apple
+banana
+orange
+['apple', 'banana', 'orange']
+'''
